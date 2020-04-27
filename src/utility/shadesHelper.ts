fed2c864933c96b04422522010054d380c532598
Ron Martinez <ramartin.net@gmail.com>
2020-04-27 09:57:05 -0400

    wip

--- src/utility/shadesHelper.ts
+++ src/utility/shadesHelper.ts
@@ -25,12 +25,12 @@ const mod = (path: any[]) => (f: (a: any) => any) => (state: any): any => {
 
 const set = (path: any[]) => (value: any) => (state: any): any => {
   switch (path.length) {
-    case 1: return shades.mod (path[0]) (value) (state)
-    case 2: return shades.mod (path[0], path[1]) (value) (state)
-    case 3: return shades.mod (path[0], path[1], path[2]) (value) (state)
-    case 4: return shades.mod (path[0], path[1], path[2], path[3]) (value) (state)
-    case 5: return shades.mod (path[0], path[1], path[2], path[3], path[4]) (value) (state)
-    case 6: return shades.mod (path[0], path[1], path[2], path[3], path[4], path[5]) (value) (state)
+    case 1: return shades.set (path[0]) (value) (state)
+    case 2: return shades.set (path[0], path[1]) (value) (state)
+    case 3: return shades.set (path[0], path[1], path[2]) (value) (state)
+    case 4: return shades.set (path[0], path[1], path[2], path[3]) (value) (state)
+    case 5: return shades.set (path[0], path[1], path[2], path[3], path[4]) (value) (state)
+    case 6: return shades.set (path[0], path[1], path[2], path[3], path[4], path[5]) (value) (state)
   }
 }
 

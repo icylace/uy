30c34733ee1b302720ba4fef9b03e47098be137e
Ron Martinez <ramartin.net@gmail.com>
2020-04-27 15:11:27 -0400

    wip

--- src/utility/shadesHelper.ts
+++ src/utility/shadesHelper.ts
@@ -2,6 +2,7 @@ import * as shades from "shades"
 
 const get = (path: any[]) => (state: any): any => {
   switch (path.length) {
+    case 0: return null
     case 1: return shades.get (path[0]) (state)
     case 2: return shades.get (path[0], path[1]) (state)
     case 3: return shades.get (path[0], path[1], path[2]) (state)
@@ -9,11 +10,12 @@ const get = (path: any[]) => (state: any): any => {
     case 5: return shades.get (path[0], path[1], path[2], path[3], path[4]) (state)
     case 6: return shades.get (path[0], path[1], path[2], path[3], path[4], path[5]) (state)
   }
-  return null
+  throw Error("`path` length is greater than 6.")
 }
 
 const mod = (path: any[]) => (f: (a: any) => any) => (state: any): any => {
   switch (path.length) {
+    case 0: return null
     case 1: return shades.mod (path[0]) (f) (state)
     case 2: return shades.mod (path[0], path[1]) (f) (state)
     case 3: return shades.mod (path[0], path[1], path[2]) (f) (state)
@@ -21,10 +23,12 @@ const mod = (path: any[]) => (f: (a: any) => any) => (state: any): any => {
     case 5: return shades.mod (path[0], path[1], path[2], path[3], path[4]) (f) (state)
     case 6: return shades.mod (path[0], path[1], path[2], path[3], path[4], path[5]) (f) (state)
   }
+  throw Error("`path` length is greater than 6.")
 }
 
 const set = (path: any[]) => (value: any) => (state: any): any => {
   switch (path.length) {
+    case 0: return null
     case 1: return shades.set (path[0]) (value) (state)
     case 2: return shades.set (path[0], path[1]) (value) (state)
     case 3: return shades.set (path[0], path[1], path[2]) (value) (state)
@@ -32,6 +36,7 @@ const set = (path: any[]) => (value: any) => (state: any): any => {
     case 5: return shades.set (path[0], path[1], path[2], path[3], path[4]) (value) (state)
     case 6: return shades.set (path[0], path[1], path[2], path[3], path[4], path[5]) (value) (state)
   }
+  throw Error("`path` length is greater than 6.")
 }
 
 export { get, mod, set }
--- task.sh
+++ task.sh
@@ -1,44 +1,64 @@
 #!/usr/bin/env bash
 
-_build:dev:bundle() {
+task:_snowpack() {
+  echo
+  echo "Use web modules created by Snowpack."
+
+  local modules=($(jq --raw-output '.imports | keys_unsorted | @sh' ./web_modules/import-map.json))
+
+  local changes=()
+  for module in "${modules[@]}" ; do
+    # Trim off the first and last characters which should be single quotes.
+    local m="${module:1:-1}"
+
+    changes+=("s/'$m';$/\"\/web_modules\/$m.js\"/g")
+  done
+
+  # https://superuser.com/a/462400/959677
+  local completeChange=$(IFS=$';' ; echo "${changes[*]}")
+
+  sed -i '' "$completeChange" ./output/rollup/index.js
+}
+
+task:_build:dev:bundle() {
   rollup --config rollup.config.dev.js
-  sed -i '' "s/from 'hyperapp'/from \"\/web_modules\/hyperapp.js\"/g;s/from 'shades'/from \"\/web_modules\/shades.js\"/g" ./output/rollup/index.js
+  task:_snowpack
   cp ./output/rollup/* ./dist
 }
 
-_build:prod:bundle() {
+task:_build:prod:bundle() {
   rollup --config rollup.config.prod.js
-  sed -i '' "s/from 'hyperapp'/from \"\/web_modules\/hyperapp.js\"/g;s/from 'shades'/from \"\/web_modules\/shades.js\"/g" ./output/rollup/index.js
+  task:_snowpack
   cp ./output/rollup/* ./dist
 }
 
-build:dev() {
-  clean
+task:build:dev() {
+  task:clean
 
   echo
   echo "Compiling TypeScript for development..."
   tsc --build --incremental false
-  _build:dev:bundle
+  task:_build:dev:bundle
 
   echo
   echo "Compiling CSS for development..."
   postcss ./src/index.css --output ./dist/index.css
 }
 
-build:dev:update() {
+task:build:dev:update() {
   echo
   echo "Compiling TypeScript incrementally for development..."
   tsc --build
-  _build:dev:bundle
+  task:_build:dev:bundle
 }
 
-build:prod() {
-  clean
+task:build:prod() {
+  task:clean
 
   echo
   echo "Compiling TypeScript for production..."
   tsc --build --incremental false
-  _build:prod:bundle
+  task:_build:prod:bundle
 
   echo
   echo "Compiling CSS for production..."
@@ -60,56 +80,62 @@ build:prod() {
   cp ./output/typescript/index.d.ts ./dist
 }
 
-clean() {
+task:clean() {
   echo
   echo "Resetting the output folders of generated files..."
   rm -fr ./dist && mkdir ./dist
   rm -fr ./output && mkdir ./output
 }
 
-lint() {
+task:lint() {
   echo
   echo "Linting..."
   eslint ./src --ext .js,.jsx,.ts,.tsx
 }
 
-lint:fix() {
+task:lint:fix() {
   echo
   echo "Linting and automatically fixing as much as possible..."
   eslint ./src --ext .js,.jsx,.ts,.tsx --fix
 }
 
-lint:fix-dry-run() {
+task:lint:fix-dry-run() {
   echo
   echo "Linting and doing a dry-run of automatically fixing as much as possible..."
   eslint ./src --ext .js,.jsx,.ts,.tsx --fix-dry-run
 }
 
-prepare() {
+task:prepare() {
   echo
   echo "Preparing web modules..."
   snowpack
 }
 
-typecheck() {
+task:typecheck() {
   echo
   echo "Type-checking TypeScript code..."
   tsc --noEmit --incremental false
 }
 
-watch:postcss() {
+task:watch() {
+  # TODO:
+  # Use watchexec...
+  return
+}
+
+task:watch:postcss() {
   echo
   echo "Watching PostCSS..."
   postcss ./src/index.css --output ./dist/index.css --watch
 }
 
-watch:rollup() {
+task:watch:rollup() {
   echo
   echo "Watching Rollup..."
   rollup --config --watch
 }
 
-watch:typescript() {
+task:watch:typescript() {
   echo
   echo "Watching TypeScript..."
   tsc --watch
@@ -122,4 +148,4 @@ if [ -z "$1" ] ; then
   exit 1
 fi
 
-"$1"
+"task:$1"

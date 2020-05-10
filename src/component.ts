355519cc62f659164ba6d6b60a030b602822342d
Ron Martinez <ramartin.net@gmail.com>
2020-05-10 03:16:55 -0400

    wip

--- src/component.ts
+++ src/component.ts
@@ -4,7 +4,7 @@ import { get, set } from "./utility/shadesHelper"
 // component :: (Options -> [String] -> Object -> VNode) -> Object -> [String] -> State -> VNode
 const component = (f: Function) => (options: object) => (path: string[]) => <S>(state: State<S>): object => {
   const data = get (path) (state)
-  const update = (state: any, value: any): any => set ([...path, "value"]) (value) (state)
+  const update = (state: State<S>, value: any): any => set ([...path, "value"]) (value) (state)
   return f ({ update, path, ...options }) (data)
 }
 

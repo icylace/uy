056542305bf5c83c43726fd0842eaec3332798f1
Ron Martinez <ramartin.net@gmail.com>
2020-10-06 10:49:29 -0400

    wip

--- src/control/textbox.ts
+++ src/control/textbox.ts
@@ -1,37 +1,22 @@
 import type { ClassProp, State, Transform, VDOM } from "hyperapp"
-// import type { Path } from "../utility/shadesHelper"
 
 import cc from "classcat"
 import { input } from "ntml"
-// import { get, set } from "../utility/shadesHelper"
 import { box } from "../container/box"
 
 export type TextboxOptions = {
   class?: ClassProp
   disabled?: boolean
   locked?: boolean
-  // update: Transform<S>
 }
 
 export type TextboxData = {
   value: string
 }
 
-const init = (value: string): TextboxData => {
-  return { value }
-}
-
-const restate = (getter, setter) => (f) => (state) => setter(state, f(getter(state)))
-
-// getter: (state) => get(path)(state) as TextboxData,
-// setter: (state, value) => set(path)(value)(state),
-
 export type TextboxSettings<S> = {
   getter: (state: State<S>) => TextboxData
   setter: (state: State<S>, value: TextboxData["value"]) => State<S>
-  // TODO:
-  // - `update` probably not needed for textboxes since it should just be `identity`
-  // update: Transform<S>
 }
 
 export type TextboxModel<S> = {
@@ -43,21 +28,6 @@ export type TextboxWiring<S> = {
   data: (state: State<S>) => TextboxData
   update: (state: State<S>, value: string) => State<S>
   model: (state: State<S>) => TextboxModel<S>
-}
-
-const wire = <S>(settings: TextboxSettings<S>): TextboxWiring<S> => {
-  const { getter, setter } = settings
-  const mod = restate(getter, setter)
-  const update = mod(settings.update)
-  return {
-    update: setter,
-    data: getter,
-    model: (state) => ({
-      // update,
-      update: setter,
-      data: getter(state),
-    }),
-  }
 }
 
 const Textbox = <S>(options: TextboxOptions, model: TextboxModel<S>): VDOM<S> => {
@@ -79,19 +49,26 @@ const Textbox = <S>(options: TextboxOptions, model: TextboxModel<S>): VDOM<S> =>
   ])
 }
 
-// const textbox = <S>(options: TextboxOptions<S>) => (path: Path) => {
-//   return (state: State<S>): VDOM<S> => {
-//     const data = get(path)(state) as TextboxData
-//     return rawTextbox({
-//       ...options,
-//       update: (state: State<S>, value: any): State<S> => {
-//         return set([...path, "value"])(value)(state)
-//       },
-//     }, data)
-//   }
-// }
+Textbox.init = (value: string): TextboxData => {
+  return { value }
+}
 
-Textbox.init = init
-Textbox.wire = wire
+// const restate = (getter, setter) => (f) => (state) => setter(state, f(getter(state)))
+
+Textbox.wire = <S>(settings: TextboxSettings<S>): TextboxWiring<S> => {
+  const { getter, setter } = settings
+  // const mod = restate(getter, setter)
+  // const update = mod(settings.update)
+  return {
+    update: setter,
+    data: getter,
+    model: (state) => ({
+      // update,
+      update: setter,
+      data: getter(state),
+    }),
+  }
+}
+
 
 export { Textbox }

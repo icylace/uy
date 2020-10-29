d17166674c9564c9a989ab232458d9cfcfeaed89
Ron Martinez <ramartin.net@gmail.com>
2020-10-29 18:32:08 -0400

    wip

--- src/component.ts
+++ src/component.ts
@@ -1,7 +1,13 @@
 export type Wiring<R extends Record<string, any>, D> = Readonly<{
-  data: (r: R) => D
-  update: (r: R, x: D) => R
+  get: (r: R) => D
+  mod: (r: R, x: D) => R
+  set: (r: R, x: D) => R
 }>
+
+// export type Wiring<R extends Record<string, any>, D> = Readonly<{
+//   data: (r: R) => D
+//   update: (r: R, x: D) => R
+// }>
 
 // const wire = <R extends Record<string, any>, D>
 //   (prop: string, context?: Wiring<R, D>): Wiring<R, D> => ({
@@ -15,11 +21,11 @@ export type Wiring<R extends Record<string, any>, D> = Readonly<{
 
 const wire = <R extends Record<string, any>, D>
   (prop: string, context?: Wiring<R, D>): Wiring<R, D> => ({
-    data: (r) => context
-      ? (context.data(r) as Record<string, any>)[prop]
+    get: (r) => context
+      ? (context.get(r) as Record<string, any>)[prop]
       : r[prop],
-    update: (r, x) => context
-      ? context.update(r, { ...context.data(r), [prop]: x })
+    set: (r, x) => context
+      ? context.set(r, { ...context.get(r), [prop]: x })
       : { ...r, [prop]: x },
   })
 

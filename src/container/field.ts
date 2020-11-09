8a02c128022d1ae271b2f80599f118797189d723
Ron Martinez <ramartin.net@gmail.com>
2020-11-09 00:13:38 -0500

    wip

--- src/container/field.ts
+++ src/container/field.ts
@@ -1,20 +1,18 @@
-import type { State, VDOM } from "hyperapp"
+import type { ClassProp, State, VDOM } from "hyperapp"
 import type { Content } from "ntml"
-import type { ComponentOptions } from "../types"
+import type { ContentView } from "../types"
 
 import cc from "classcat"
 import { label } from "ntml"
 import { box } from "./box"
 
 export type FieldOptions = {
-  [_: string]: unknown
+  class?: ClassProp
   disabled?: boolean
   locked?: boolean
 }
 
-export type Component<S> = (options: ComponentOptions) => (state: State<S>) => VDOM<S>
-
-const field = <S>(title: Content<S>, f: Component<S>, options: FieldOptions) => (state: State<S>): VDOM<S> => {
+const field = <S>(title: Content<S>, options: FieldOptions, views: ContentView<S>[]) => (state: State<S>): VDOM<S> => {
   const { disabled, locked, ...etc } = options
   return box("uy-field", [
     label(
@@ -24,7 +22,7 @@ const field = <S>(title: Content<S>, f: Component<S>, options: FieldOptions) => 
       },
       [
         title,
-        f({ disabled, locked, ...etc })(state),
+        ...views.map((view) => view(state)),
       ],
     ),
   ])
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,4 @@
-import type { ClassProp, State } from "hyperapp"
+import type { State } from "hyperapp"
 import type { Content } from "ntml"
-
-export type ComponentOptions = {
-  [_: string]: unknown
-  class?: ClassProp
-  disabled?: boolean
-  locked?: boolean
-}
 
 export type ContentView<S> = (state: State<S>) => Content<S>
